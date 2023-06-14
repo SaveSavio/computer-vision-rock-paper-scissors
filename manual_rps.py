8a0765bfc81022b8ba171c39f8182e8bbc5419f1
Saverio Tufano <saverio.tufano@gmail.com>
2023-06-14 23:00:46 +0100

    manual_rps, first draft of functions created

--- manual_rps.py
+++ manual_rps.py
@@ -1,5 +1,20 @@
 def get_computer_choice():
-    pass
+    import random
+    variables = ['rock', 'paper', 'scissors']
+    computer_choice =  random.choice(variables)
+    return computer_choice
 
 def get_user_choice():
-    pass
+    variables = ['rock', 'paper', 'scissors']
+    while True:
+        choice = input("Choose between rock (0), paper(1), or scissors(2)")
+        if choice == 0:
+            player_choice = 'rock'
+        elif choice == 1:
+            player_choice = 'paper'
+        elif choice == 2:
+            player_choice = 'scissors'
+        else:
+            print("Invalid choise. Please, enter 0, 1 or 2")
+            return
+        return player_choice
